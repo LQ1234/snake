99f3e61b694b4fc359f19355b0ae71735749f9a5
andreas74 <Bubbydebub@gmail.com>
2020-11-25 13:23:52 -0500

    Create userInterface.py

---
+++ userInterface.py
@@ -0,0 +1,16 @@
+import pygame
+import game_logic
+
+class UserInterface:
+	#use pygame library to set refresh rate and open window
+	
+	def __init__(self):
+		self.snake = Snake()
+		self.logic = GameLogic()
+	
+	def get_keyboard_input(): #give game_logic key input
+
+	def blit_screen(): #use blit method for pygame to display objects
+
+	def run_game():
+		while true: #main game loop
