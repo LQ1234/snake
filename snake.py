036b6a28b7637093b483224a2dc6e8191417b4d2
andreas74 <Bubbydebub@gmail.com>
2020-11-28 15:54:55 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,27 +1,19 @@
-class ClassName(object):
-    """docstring for ."""
-
-    def __init__(self, arg):
-        super(, self).__init__()
-        self.arg = arg
-
 class Snake():
     
-    def __init__(self, initial_position)
-        self.body = [initial_position] #we are not using linked list anymore
-
+    def __init__(self, initial_position):
+        self.body = [initial_position] #store position as tuple of x, y
 
     def move(self, position):
         pass
 
-    def add_segment(position):#adds to beginning
-        body.insert(0, position)
+    def add_segment(self, position):#adds to beginning
+        self.body.insert(0, position)
 
-    def remove_segment():#removes from end
-        body.pop(len(body) - 1)
+    def remove_segment(self):#removes from end
+        self.body.pop(len(self.body) - 1)
 
-    def size_of_snake():
-        return len(body)
+    def size_of_snake(self):
+        return len(self.body)
 
-    def get_head():
-        return body[0]
+    def get_head(self):
+        return self.body[0]
