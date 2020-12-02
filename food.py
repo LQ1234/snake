0399326bc783c979b748bf337f187bb938c0354e
amylnikov30 <amylnikov30@gmail.com>
2020-12-02 12:13:26 -0500

    [Additions]Empty move and draw method to snake, Vector2 as position [Bugs] [Removals]

--- food.py
+++ food.py
@@ -5,4 +5,5 @@ class Food(pygame.sprite.Sprite):
     def __init__(self, position, ui):
         self.position=position
         self.ui = ui
-        pygame.sprite.Sprite.__init__(ui.sprites, ui.food)
+        self.groups = ui.sprites, ui.food
+        pygame.sprite.Sprite.__init__(self, ui.groups)
--- snake.py
+++ snake.py
@@ -1,5 +1,5 @@
 import pygame
-
+import userInterface
 
 
 class Snake(pygame.sprite.Sprite):
@@ -7,7 +7,11 @@ class Snake(pygame.sprite.Sprite):
     def __init__(self, initial_position, ui):
         self.body = [initial_position] #store position as tuple of x, ys
         self.ui = ui
-        pygame.sprite.Sprite.__init__(ui.sprites, ui.snake_segment)
+        self.groups = ui.sprites, ui.snake_segment
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.rect = pygame.Rect(initial_position[0], initialPosition[1], userInterface.x_spacing, userInterface.y_spacing)
+        self.position = Vector2(initialPosition[0], initialPosition[1])
+
 
     def move(self, position):
         pass
@@ -23,3 +27,9 @@ class Snake(pygame.sprite.Sprite):
 
     def get_head(self):
         return self.body[0]
+
+    def draw(self):
+        pass # snake needs to have move done to do update and draw
+
+    def update(self):
+        pass
